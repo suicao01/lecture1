d145d2eb2d02f91e06d17404c95c8e91b7e7000a
水饺 <126674519+suicao01@users.noreply.github.com>
2023-09-03 21:08:36 +0700

    Untitled-1.cpp

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -65,3 +65,79 @@ int main(){
         cout<<s[i];
     }
 }
+
+
+// #include <iostream>
+
+// using namespace std;
+
+// class Point{
+//     public:
+//     double x,y;
+//     Point(double x_,double y_){
+//         x=x_;
+//         y=y_;
+//     }
+//     Point(){
+//         x=0;
+//         y=0;
+//     }
+//     void input(){
+//         cin>>x>>y;
+//     }
+// };
+
+// class Line:public Point{
+//     public:
+//     double a,b,c;
+// Line (Point A,Point B){
+//     this->a=B.y-A.y;
+//     this->b=A.x-B.x;
+//     this->c=this->a*(A.x)+this->b*(A.y);
+// }
+// };
+
+// bool isSame(Point A,Point B,Point C,Point D){
+//     Line AB(A,B);
+// Line CD(C,D);
+//     if (AB.a==CD.a && AB.b==CD.b){
+//         return true;
+//     }
+//     else return false;
+// }
+
+// Point intersectionPoint(Point A,Point B,Point C,Point D){
+// Line AB(A,B);
+// Line CD(C,D);
+// Point res;
+// double det=AB.a*CD.b-CD.a*AB.b;
+
+// if (det==0){
+//     res.x=__FLT_MAX__;
+//     res.y=__FLT_MAX__;
+// }
+// else{
+//     res.x=(CD.b*AB.c-AB.b*CD.c)/det;
+//     res.y=(AB.a*CD.c-CD.a*AB.c)/det;
+
+// }
+// return res;
+// }
+
+// int main(){
+// //Point A(0,0),B(1,1),C(0,1),D(1,0);
+// Point A,B,C,D;
+// A.input();
+// B.input();
+// C.input();
+// D.input();
+// Point res=intersectionPoint(A,B,C,D);
+// if (isSame(A,B,C,D)){
+//     cout<<"MANY";
+// }
+// else if (res.x==__FLT_MAX__ && res.y==__FLT_MAX__){
+//     cout<<"NO";
+// }
+// else{
+// cout<<res.x<<" "<<res.y;}
+// }
